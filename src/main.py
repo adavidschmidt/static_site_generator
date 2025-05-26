f5491aa1468dbeb594e7da1b058b0dbcd74746d8
andrew <122319655+adavidschmidt@users.noreply.github.com>
2025-05-26 13:12:05 -0400

    make changes to current main file

--- src/main.py
+++ src/main.py
@@ -2,23 +2,41 @@ from textnode import TextNode, TextType
 import os
 import shutil
 
-def copy_static():
-	pass
-def get_static_files(current_filetree, current_path):
+
+def make_public():
+	if not os.path.exists("public"):
+		os.mkdir("public")
+
+def get_path_files(path):
+	return os.listdir(path)
+
+def get_static_files(path_files, current_path):
 	file_paths = []
-	for dir, content in current_filetree.items():
-		new_path = f"{current_path}/{dir}"
-		if content is None:
-			file_paths.extend(new_path)
+	for item in path_files:
+		item_path = f"{current_path}/{item}"
+		if os.path.isfile(item_path):
+			file_paths.append(item_path)
 		else:
-			file_paths.extend(get_static_files(content, new_path))
+			file_paths.append(item_path)
+			file_paths.extend(get_static_files(os.listdir(item_path), item_path))
 	return file_paths
 
+def copy_files(source_items, target_directory):
+	for item in source_items:
+		if os.path.isfile(item):
+			shutil.copy(item, target_directory)
+		else:
+			directory = item.strip("static")
+			os.mkdir(f"{target_directory}/{directory}")
+
 def main():
-	if not os.path.exists("public/"):
-		os.mkdir("public")
+	make_public()
+	if os.path.exists("static/"):
+		path_files = get_path_files("static/")
 	else:
-		print("exists")
+		raise Exception("static directory does not exist")
+	static_files = get_static_files(path_files, "static")
+	copy_files(static_files, "public/")
 	#to run at the end of the function to copy the items from the directory.
 	#shutil.rmtree("static/")
 
