3722573c08c766d6893387c1c824029b4fc913c2
andrew <122319655+adavidschmidt@users.noreply.github.com>
2025-05-25 21:25:26 -0400

    add some parts of the assignment

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_static():
+	pass
+def get_static_files(current_filetree, current_path):
+	file_paths = []
+	for dir, content in current_filetree.items():
+		new_path = f"{current_path}/{dir}"
+		if content is None:
+			file_paths.extend(new_path)
+		else:
+			file_paths.extend(get_static_files(content, new_path))
+	return file_paths
 
 def main():
-	text_node = TextNode("sample text", TextType.TEXT, "boot.dev")
-	print(text_node)
+	if not os.path.exists("public/"):
+		os.mkdir("public")
+	else:
+		print("exists")
+	#to run at the end of the function to copy the items from the directory.
+	#shutil.rmtree("static/")
 
 main()
