36517df6dd811b784987973422a55a873001472b
Andrew <122319655+adavidschmidt@users.noreply.github.com>
2025-05-27 14:56:52 -0400

    minor changes

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ template_path = "template.html"
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    print(basepath)
     make_public(target_path)
     copy_files_recursive(static_path, target_path)
     generate_page_recursive(content_path, template_path, target_path, basepath)
